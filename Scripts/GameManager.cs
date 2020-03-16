728aacdaf6fcb66f5afe57341b3fb090505ad201
Syberian <mixux98@gmail.com>
2020-03-16 21:37:04 +0100

    Init scripts

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    private void Awake()
+    {
+        if (!Instance)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
---
+++ Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask clickableLayer;
+    [SerializeField] private Camera cam = null;
+    private NavMeshAgent _playerAgent = null;
+
+    private void Awake()
+    {
+        _playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray r = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(r, out var hitInfo, 1000, clickableLayer))
+            {
+                _playerAgent.SetDestination(hitInfo.point);
+            }
+        }
+    }
+}
