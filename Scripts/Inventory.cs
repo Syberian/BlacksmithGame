b084c4926193bd1e3e436c71d966600b1293c6ec
Syberian <mixux98@gmail.com>
2020-03-17 22:44:56 +0100

    inventory

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+    
+public class Inventory : MonoBehaviour
+{
+    public List<Item> inventory = new List<Item>();
+    public static event Action<Item> OnItemPickedUp = delegate { };
+    public void AddItem(Item itemToAdd)
+    {
+        if (inventory.Count < 3)
+        {
+            inventory.Add(itemToAdd);
+            OnItemPickedUp?.Invoke(itemToAdd);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        OnItemPickedUp = delegate{  };
+    }
+}
+
+[CreateAssetMenu]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+    public Mesh mesh;
+}
